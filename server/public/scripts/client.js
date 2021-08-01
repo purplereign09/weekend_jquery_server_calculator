458f8d6d71ca2e37bb29b4f77a58a8e6bfa846c5
Patty Baraibar <pattybaraibar@msn.com>
2021-07-31 19:26:24 -0500

    server side functions

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,21 +2,52 @@ $(document).ready(onReady);
 console.log('hi');
 
 //Global array collecting input value one
-const userValue = { }
+let userValue = { }
+//set a global variable to a button on the interface to be able to track it
 let buttonClicked;    
 
 //getting our jQuery function installed to the page
 function onReady(){
-
+//on the click of the class button we are tracking the attribute of the ID of the the operator selected by the user
     $('.btn').click(function (){
         buttonClicked = $(this).attr('id');
         console.log(buttonClicked);
     });
 
-    $('document').on('click', 'equalsBttn', postNum);
+    $('#equalsBttn').click(function (){
+        postNum();
+    });
 
-}
+};
 
 function postNum(){
-
+    console.log('here');
+    const numOne = $('#numOne').val();
+    const numTwo = $('#numTwo').val();
+    console.log(numOne);
+    console.log(numTwo);
+    userValue = {
+        val1: numOne,
+        val2: numTwo,
+        operator: buttonClicked
+        
+    }
+    console.log(userValue);
+    //POST
+    $.ajax({ 
+        method: 'POST',
+        url:'/calculate',
+        data: JSON.stringify(userValue),
+        // data: {JSON.stringify(guesses)},
+        contentType: 'application/json'
+    }).then(function(response) {
+        console.log('we are not getting here');
+        console.log('POST / userValue', response);
+    }).catch((error) => {
+        console.log('failed', error);
+        $('body').prepend('<h2>ERROR</h2>');
+    });
 }
+
+
+
--- server/server.js
+++ server/server.js
@@ -10,11 +10,21 @@ app.use(bodyParser.urlencoded({extended:true}));
 //This is where our client.js file lives
 app.use(express.static('server/public'));
 
-
-
-
+app.post('/calculate', function(req,res) {
+let userValue = req.body;
+console.log(req, res);
+});
 
 app.listen(PORT, () => {
   //console.log ('Server is running on port', PORT)
   console.log (`Connect to: http://localhost:${PORT}`);
 })
+
+
+//if ' + ' req.operator === 'additionBttn') {
+  result = req.val1 + val2 {
+    }  else if req.operator === 'subtractionBttn')....
+
+
+
+    
